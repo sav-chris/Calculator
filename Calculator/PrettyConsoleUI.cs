e908b78b14887798c4783ca8f0fd5c3a7aac7f25
sav <christopher.savini@gmail.com>
2022-04-06 15:12:51 +1000

    Pretty console

---
+++ Calculator/PrettyConsoleUI.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class PrettyConsoleUI : CalculatorUI
+    {
+        private Controller Controller { get; }
+
+        private void SetColours()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        public void DisplayOperations()
+        {
+            SetColours();
+
+            Console.WriteLine("Choose an operation: ");
+            int itemNo = 1;
+            foreach (Operation operation in this.Controller.Operations)
+            {
+                string itemText = (itemNo++).ToString();
+                Console.WriteLine(itemText + ". " + operation.Name);
+            }
+
+            Console.WriteLine((itemNo++).ToString() + ". Exit");
+        }
+
+        public double? RequestArgument()
+        {
+            SetColours();
+
+            Console.WriteLine("Provide Argument");
+            string line = Console.ReadLine();
+            if (double.TryParse(line, out double arg))
+            {
+                return arg;
+            }
+            return null;
+        }
+        public void ShowError()
+        {
+            SetColours();
+            Console.WriteLine("Invalid input");
+            Console.WriteLine(" ");
+        }
+        public void ShowResult(double result)
+        {
+            SetColours();
+            Console.WriteLine("The result is:");
+            Console.WriteLine(result.ToString());
+            Console.WriteLine(" ");
+        }
+
+
+        public int SelectOperationIndex()
+        {
+            string selection = Console.ReadLine();
+            if (!Int32.TryParse(selection, out int selectionIndex))
+            {
+                selectionIndex = this.Controller.OperationCount + 1;
+            }
+            return selectionIndex;
+        }
+
+        public void Run()
+        {
+            this.Controller.Run(this);
+        }
+
+        public PrettyConsoleUI(Controller controller)
+        {
+            this.Controller = controller;
+        }
+    }
+}
